03b45dc744e1ca86dcb048a388846b5ae92f28d2
AugustoCoding <audecan@gmail.com>
2025-07-17 10:58:41 +0200

    detele

--- streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-from plotly import graph_objs as go
-
-st.title("Sustain 4.0 BioEngine")
-st.write("Welcome to the Sustain 4.0 BioEngine Streamlit app!")
